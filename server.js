6ff902d7b3593413d66465b1a4768fcfce6bc7cc
harlita <harlitad@gmail.com>
2021-05-02 00:48:36 +0700

    set emailQueue and router bull-board

--- server.js
+++ server.js
@@ -2,17 +2,21 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const { readFileSync } = require("fs");
-const { sendMail } = require("./serviceEmail");
+const { emailQueue, routerBoard } = require("./emailQueue");
+const options = {
+  attempts: 2, // if failed, 2 times retry
+};
 
 function readFile(path) {
   const data = readFileSync(path);
   return JSON.parse(data.toString());
 }
 
+app.use("/admin/queues", routerBoard);
 app.get("/", async (req, res, next) => {
-  const list_email = readFile('./data.json').list_email;
-  list_email.forEach(e => {
-      sendMail(e)
+  const list_email = readFile("./data.json").list_email;
+  list_email.forEach((e) => {
+    emailQueue.add({ email: e }, options);
   });
   res.json("email sent !");
 });
